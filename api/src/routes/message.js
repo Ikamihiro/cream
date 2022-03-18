27b294891aee03ebee12a620987a8688293614b5
João G. Silva <joogabrielsntn66@gmail.com>
2022-03-17 20:32:17 -0400

    Declarando a nova rota OK

--- api/src/routes/message.js
+++ api/src/routes/message.js
@@ -6,6 +6,9 @@ const messageRouter = express.Router()
 // Pega as mensagens do Chat
 messageRouter.get("/:chatId", MessageController.getAll)
 
+// Pega as ultimas mensagens
+messageRouter.get("/lastMessages", MessageController.getLastMessages)
+
 // Pega uma mensagem especificada
 messageRouter.get("/show/:messageId", MessageController.getById)
 
